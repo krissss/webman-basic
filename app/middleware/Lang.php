5507f73fa0cc4977388b1269c876c9a8fb37685f
kriss <462679766@qq.com>
2022-05-22 20:36:04 +0800

    feature: lang support header

--- app/middleware/Lang.php
+++ app/middleware/Lang.php
@@ -1,18 +1,21 @@
 <?php
+
 namespace app\middleware;
 
-use Webman\MiddlewareInterface;
-use Webman\Http\Response;
 use Webman\Http\Request;
+use Webman\Http\Response;
+use Webman\MiddlewareInterface;
 
 /**
  * 语言设置
  */
 class Lang implements MiddlewareInterface
 {
-    public function process(Request $request, callable $handler) : Response
+    public function process(Request $request, callable $handler): Response
     {
         if ($lang = session('lang')) {
+            locale($lang);
+        } elseif ($lang = $request->header('Accept-Language')) {
             locale($lang);
         }
         /** @var Response $response */
