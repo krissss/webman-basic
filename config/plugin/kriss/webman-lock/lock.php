392b52884af984506b8943941bbdc8ef64d1338c
kriss <462679766@qq.com>
2022-05-24 16:45:50 +0800

    feature: update lock

---
+++ config/plugin/kriss/webman-lock/lock.php
@@ -0,0 +1,29 @@
+<?php
+
+use Symfony\Component\Lock\Store\FlockStore;
+use Symfony\Component\Lock\Store\RedisStore;
+
+return [
+    'storage' => 'redis', // file/redis， file 不支持 ttl
+    'storage_configs' => [
+        'file' => [
+            'class' => FlockStore::class,
+            'construct' => [
+                'lockPath' => runtime_path() . '/lock',
+            ],
+        ],
+        'redis' => [
+            'class' => RedisStore::class,
+            'construct' => function() {
+                return [
+                    'redis' => \support\Redis::connection('default')->client(),
+                ];
+            },
+        ],
+    ],
+    'default_config' => [
+        'ttl' => 300, // 默认锁超时时间
+        'auto_release' => true, // 是否自动释放，建议设置为 true
+        'prefix' => 'lock_', // 锁前缀
+    ],
+];
--- support/facade/Locker.php
+++ support/facade/Locker.php
@@ -5,8 +5,8 @@ namespace support\facade;
 use Symfony\Component\Lock\LockInterface;
 
 /**
- * @method static LockInterface test($key = null, $ttl = 300, $autoRelease = true)
+ * @method static LockInterface test(?string $key = null, ?float $ttl = null, ?bool $autoRelease = null, ?string $prefix = null)
  */
-class Locker extends \app\components\Locker
+class Locker extends \Kriss\WebmanLock\Locker
 {
 }
