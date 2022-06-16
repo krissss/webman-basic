dbb2ff499331afc6c56902c54f78fbb45c74f6a7
kriss <462679766@qq.com>
2022-06-16 09:47:13 +0800

    feature: add ide-helper

---
+++ .phpstorm.meta.php
@@ -0,0 +1,12 @@
+<?php
+// @formatter:off
+
+namespace PHPSTORM_META {
+
+    override(\support\Container::make(0), map([
+        '' => '@',
+    ]));
+    override(\support\Container::get(0), map([
+        '' => '@',
+    ]));
+}
