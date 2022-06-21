78650528337162b7aff2606aa3fbe0b3c2efaf93
kriss <462679766@qq.com>
2022-06-21 11:33:59 +0800

    feature: redis-queue 支持 prefix （需要等待依赖升级）

--- config/plugin/webman/redis-queue/redis.php
+++ config/plugin/webman/redis-queue/redis.php
@@ -5,6 +5,7 @@ return [
         'options' => [
             'auth' => get_env('REDIS_PASSWORD'),       // 密码，字符串类型，可选参数
             'db' => get_env('REDIS_DB_QUEUE', 0),            // 数据库
+            'prefix' => get_env('REDIS_PREFIX_QUEUE', config('app.name') . ':queue:'),
             'max_attempts' => 5, // 消费失败后，重试次数
             'retry_seconds' => 5, // 重试间隔，单位秒
         ]
