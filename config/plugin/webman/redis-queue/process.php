da4ac63e054f44bc3550423b4b1da432ea26d12e
kriss <462679766@qq.com>
2022-06-21 13:45:55 +0800

    feature: redis-queue 支持仅启用但不启用消费者

--- config/plugin/webman/redis-queue/process.php
+++ config/plugin/webman/redis-queue/process.php
@@ -1,9 +1,13 @@
 <?php
 
+if (!get_env('QUEUE_CONSUMER_ENABLE', true)) {
+    return [];
+}
+
 return [
-    'consumer'  => [
-        'handler'     => Webman\RedisQueue\Process\Consumer::class,
-        'count'       => get_env('QUEUE_REDIS_CONSUMER_COUNT', 8), // 可以设置多进程同时消费
+    'consumer' => [
+        'handler' => Webman\RedisQueue\Process\Consumer::class,
+        'count' => get_env('QUEUE_REDIS_CONSUMER_COUNT', 8), // 可以设置多进程同时消费
         'constructor' => [
             // 消费者类目录
             'consumer_dir' => app_path() . '/queue/redis'
