274198ae9a0e41c74ab57402cf97cc4483e7a9c5
kriss <462679766@qq.com>
2022-09-09 14:31:13 +0800

    docs: add doc

--- app/api/route.php
+++ app/api/route.php
@@ -3,3 +3,6 @@
 use app\api\controller\OpenApiController;
 
 OpenApiController::registerRoute();
+
+// 请勿将以下注释打开使用，仅做参考使用
+//\support\facade\Route::resource('example', \app\api\controller\ExampleSourceController::class, ['name_prefix' => 'api.']);
