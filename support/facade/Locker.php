c4c68ecf0d3ee347cc204130b3a6d9286b8e7bfd
kriss <462679766@qq.com>
2022-09-18 21:36:17 +0800

    feature: change lock

--- support/facade/Locker.php
+++ support/facade/Locker.php
@@ -5,9 +5,8 @@ namespace support\facade;
 use Symfony\Component\Lock\LockInterface;
 
 /**
- * 使用前安装依赖：composer require kriss/webman-lock
  * @method static LockInterface test(?string $key = null, ?float $ttl = null, ?bool $autoRelease = null, ?string $prefix = null)
  */
-class Locker extends \Kriss\WebmanLock\Locker
+class Locker extends \WebmanTech\SymfonyLock\Locker
 {
 }
