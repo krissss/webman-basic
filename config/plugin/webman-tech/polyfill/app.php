1fb8d0d9f4db66f04899ade76b992efc012d1421
kriss <462679766@qq.com>
2022-10-19 17:46:11 +0800

    feature: update dep

--- config/plugin/webman-tech/polyfill/app.php
+++ config/plugin/webman-tech/polyfill/app.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Contracts\Filesystem\Factory as FilesystemFactory;
 use Illuminate\Contracts\Validation\Factory as ValidationFactory;
-use support\facade\Storage;
-use support\facade\Validator;
+use support\Container;
 
 return [
     'enable' => true,
@@ -12,13 +11,31 @@ return [
          * 如果用到 Laravel UploadedFile 中的 store 或 storeAs 相关方法，需要提供 filesystemFactory 实现
          */
         'filesystem' => function (): FilesystemFactory {
-            return Storage::instance();
+            if (class_exists(\WebmanTech\LaravelFilesystem\Facades\Storage::class)) {
+                return \WebmanTech\LaravelFilesystem\Facades\Storage::instance();
+            }
+            if (!Container::has(FilesystemFactory::class)) {
+                throw new InvalidArgumentException('请先配置 FilesystemFactory 实现');
+            }
+            return Container::get(FilesystemFactory::class);
         },
         /**
          * 如果用到 Laravel Request 中的 validate，需要提供 validationFactory 实现
          */
         'validation' => function (): ValidationFactory {
-            return Validator::instance();
+            if (class_exists(\WebmanTech\LaravelValidation\Facades\Validator::class)) {
+                return \WebmanTech\LaravelValidation\Facades\Validator::instance();
+            }
+            if (function_exists('validator')) {
+                $validator = validator();
+                if ($validator instanceof ValidationFactory) {
+                    return $validator;
+                }
+            }
+            if (!Container::has(ValidationFactory::class)) {
+                throw new InvalidArgumentException('请先配置 ValidationFactory 实现');
+            }
+            return Container::get(ValidationFactory::class);
         }
     ],
 ];
