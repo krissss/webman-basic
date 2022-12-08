5ce40d920a92eac4d53774acec91e948380fe7bf
kriss <462679766@qq.com>
2022-12-08 13:22:09 +0800

    feature: add UuidPrimaryKey

---
+++ app/model/traits/UuidPrimaryKey.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\model\traits;
+
+use Ramsey\Uuid\Uuid as Generator;
+
+/**
+ * https://github.com/goldspecdigital/laravel-eloquent-uuid
+ */
+trait UuidPrimaryKey
+{
+    /**
+     * Boot uuid trait.
+     *
+     * @return void
+     */
+    protected static function bootUuidPrimaryKey()
+    {
+        static::creating(function (self $model) {
+            if (!$model->{$model->getKeyName()} || !Generator::isValid($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = Generator::uuid4()->toString();
+            }
+        });
+    }
+
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Get the auto-incrementing key type.
+     *
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'uuid';
+    }
+}
