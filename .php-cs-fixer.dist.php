f20f29d66b8cfa72ffd1bff17334e0f9e74c4964
kriss <462679766@qq.com>
2022-12-28 16:24:43 +0800

    feature: more fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,6 +8,16 @@ $config = new PhpCsFixer\Config();
 return $config
     ->setLineEnding(PHP_EOL)
     ->setCacheFile(__DIR__ . '/runtime/.php-cs-fixer.cache')
+    ->setFinder(PhpCsFixer\Finder::create()
+        ->in([
+            __DIR__ . '/app',
+            __DIR__ . '/support/facade',
+            __DIR__ . '/process/Task',
+        ])
+        ->notPath([
+            __DIR__ . '/app/command/TestCommand.php',
+        ])
+    )
     ->setRules([
         '@PSR12' => true,
         //'@PSR12:risky' => true,
@@ -20,12 +30,11 @@ return $config
         // @PHP74Migration:risky 的调整
         'declare_strict_types' => false, // 不强制定义 declare(strict_types=1);
         'void_return' => false, // 不强制添加 void 返回类型
+        // 其他
+        'no_unused_imports' => true, // 去除无用的 import
+        'native_function_casing' => true, // php 函数使用正确的大小写
+        'standardize_not_equals' => true, // <> => !=
+        'cast_spaces' => ['space' => 'none'], // 强制类型转化无空格
+        'concat_space' => ['spacing'=>'one'], // 连接字符串之间要一个空格
     ])
-    ->setFinder(PhpCsFixer\Finder::create()
-        ->in([
-            __DIR__ . '/app',
-            __DIR__ . '/support/facade',
-            __DIR__ . '/process/Task',
-        ])
-    )
     ;
