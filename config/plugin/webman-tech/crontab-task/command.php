1d3c5e656e0e06e5e690f2dfa1cf5a4d4d38ffde
kriss <462679766@qq.com>
2023-05-07 20:18:48 +0800

    feature: separate crontab-task

---
+++ config/plugin/webman-tech/crontab-task/command.php
@@ -0,0 +1,9 @@
+<?php
+
+use WebmanTech\CrontabTask\Commands\CrontabTaskListCommand;
+use WebmanTech\CrontabTask\Commands\MakeTaskCommand;
+
+return [
+    CrontabTaskListCommand::class,
+    MakeTaskCommand::class,
+];
