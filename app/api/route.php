0f75f2b6e6215e324b89efcb16a21e3f36224e36
kriss <462679766@qq.com>
2023-05-08 18:20:01 +0800

    feature: 独立注册到单独的文件

--- app/api/route.php
+++ app/api/route.php
@@ -1,42 +1,13 @@
 <?php
 
-use OpenApi\Annotations as OA;
+use app\components\SwaggerRegister;
 use support\facade\Route;
 use WebmanTech\Auth\Middleware\Authentication;
 use WebmanTech\Auth\Middleware\SetAuthGuard;
 use WebmanTech\LaravelCache\Middleware\ThrottleRequestsFactory;
-use WebmanTech\Swagger\Swagger;
 
-(new Swagger())->registerRoute([
-    'openapi_doc' => [
-        'scan_path' => __DIR__ . '/controller',
-        'scan_exclude' => [
-            // 注释掉下面这个，可以看到 openapi 的例子
-            'ExampleSourceController.php',
-        ],
-        'modify' => function (OA\OpenApi $openapi) {
-            $openapi->info->title = config('app.name') . ' API';
-            $openapi->info->version = '1.0.0';
-            $openapi->servers = [
-                new OA\Server([
-                    'url' => '/api',
-                    'description' => 'localhost',
-                ]),
-            ];
-            if (!$openapi->components instanceof OA\Components) {
-                $openapi->components = new OA\Components([]);
-            }
-            $openapi->components->securitySchemes = [
-                new OA\SecurityScheme([
-                    'securityScheme' => 'api_key',
-                    'type' => 'apiKey',
-                    'in' => 'header',
-                    'name' => 'X-Api-Key',
-                ])
-            ];
-        }
-    ],
-]);
+// openapi 路由
+SwaggerRegister::registerRouteApi(__DIR__ . '/controller');
 
 // 请勿将以下注释打开使用，仅做参考使用
 //Route::resource('crud', \app\api\controller\ExampleSourceController::class, ['name_prefix' => 'api.']);
---
+++ app/components/SwaggerRegister.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\components;
+
+use OpenApi\Annotations as OA;
+use WebmanTech\Swagger\Swagger;
+
+class SwaggerRegister
+{
+    public static function registerRouteApi(string $scanPath)
+    {
+        (new Swagger())->registerRoute([
+            'openapi_doc' => [
+                'scan_path' => $scanPath,
+                'scan_exclude' => [
+                    // 注释掉下面这个，可以看到 openapi 的例子
+                    'ExampleSourceController.php',
+                ],
+                'modify' => function (OA\OpenApi $openapi) {
+                    $openapi->info->title = config('app.name') . ' API';
+                    $openapi->info->version = '1.0.0';
+                    $openapi->servers = [
+                        new OA\Server([
+                            'url' => '/api',
+                            'description' => 'localhost',
+                        ]),
+                    ];
+                    if (!$openapi->components instanceof OA\Components) {
+                        $openapi->components = new OA\Components([]);
+                    }
+                    $openapi->components->securitySchemes = [
+                        new OA\SecurityScheme([
+                            'securityScheme' => 'api_key',
+                            'type' => 'apiKey',
+                            'in' => 'header',
+                            'name' => 'X-Api-Key',
+                        ])
+                    ];
+                }
+            ],
+        ]);
+    }
+}
