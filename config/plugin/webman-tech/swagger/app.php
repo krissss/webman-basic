3ecc544c960b13ae71b7dd36278487eadaed65d5
kriss <462679766@qq.com>
2023-12-07 20:03:11 +0800

    feature: add env

--- config/plugin/webman-tech/swagger/app.php
+++ config/plugin/webman-tech/swagger/app.php
@@ -3,30 +3,30 @@
 return [
     'enable' => get_env('OPENAPI_ENABLE', true),
     'global_route' => [
-        /**
+        /*
          * 全局扫描的配置
          * @see \WebmanTech\Swagger\Swagger::registerGlobalRoute()
          */
         'enable' => false,
     ],
     'host_forbidden' => [
-        /**
+        /*
          * 全局的 host forbidden 配置
          * @see \WebmanTech\Swagger\Middleware\HostForbiddenMiddleware::$config
          */
-        'enable' => true,
+        'enable' => get_env('OPENAPI_HOST_FORBIDDEN', true),
         'host_white_list' => [
-            'gateway'
+            'gateway',
         ],
     ],
     'swagger_ui' => [
-        /**
+        /*
          * 全局的 swagger ui 配置
          * @see \WebmanTech\Swagger\Controller\OpenapiController::swaggerUI()
          */
     ],
     'openapi_doc' => [
-        /**
+        /*
          * 全局的 openapi doc 配置
          * @see \WebmanTech\Swagger\Controller\OpenapiController::openapiDoc()
          */
