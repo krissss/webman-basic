1a77e9faa12fd258e796e7dfcd716a8496e50e6a
kriss <462679766@qq.com>
2025-06-05 11:14:27 +0800

    feature: lock laravel version

--- config/plugin/webman-tech/laravel-console/artisan.php
+++ config/plugin/webman-tech/laravel-console/artisan.php
@@ -1,7 +1,15 @@
 <?php
 
 return [
+    /**
+     * @see \WebmanTech\LaravelConsole\Kernel::$config
+     */
+    // 自定义 Command
     'commands' => [
         // commandName
+    ],
+    'commands_scan' => [
+        'webman' => true, // 是否扫描 webman/console
+        'illuminate_database' => false, // 是否扫描 illuminate/database
     ],
 ];
