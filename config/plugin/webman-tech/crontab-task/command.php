b286633625fb85b05eae8322ba845b3277fb2e6e
kriss <462679766@qq.com>
2025-06-09 09:53:09 +0800

    feature: use monorepo

--- config/plugin/webman-tech/crontab-task/command.php
+++ config/plugin/webman-tech/crontab-task/command.php
@@ -1,9 +1,11 @@
 <?php
 
+use WebmanTech\CrontabTask\Commands\CrontabTaskExecCommand;
 use WebmanTech\CrontabTask\Commands\CrontabTaskListCommand;
 use WebmanTech\CrontabTask\Commands\MakeTaskCommand;
 
 return [
     CrontabTaskListCommand::class,
+    CrontabTaskExecCommand::class,
     MakeTaskCommand::class,
 ];
