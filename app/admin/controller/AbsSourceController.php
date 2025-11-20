36693a1573e092cb6eff01760ab40ca6e695b2bb
kriss <462679766@qq.com>
2025-11-20 15:16:32 +0800

    feature: 添加快速配置 admin 的一些常用功能

--- app/admin/controller/AbsSourceController.php
+++ app/admin/controller/AbsSourceController.php
@@ -2,6 +2,8 @@
 
 namespace app\admin\controller;
 
+//use app\model\Admin;
+//use support\facade\Auth;
 use WebmanTech\AmisAdmin\Controller\AmisSourceController;
 use WebmanTech\AmisAdmin\Repository\EloquentRepository;
 
@@ -10,6 +12,15 @@ use WebmanTech\AmisAdmin\Repository\EloquentRepository;
  */
 abstract class AbsSourceController extends AmisSourceController
 {
+//    protected ?array $defaultDialogConfig = [
+//        'size' => 'lg',
+//    ];
+
+//    public function __construct()
+//    {
+//        $this->hiddenDestroy = !get_env('ADMIN_CAN_SUPERADMIN_DESTROY', false) || Auth::getId() != Admin::SUPER_ADMIN_ID;
+//    }
+
     /**
      * {@inheritdoc}
      */
